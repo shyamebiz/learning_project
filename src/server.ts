b3b129ec67cbdab7d093b886b61a250a01a4bbab
EBIZNEEDS\sgupta <sgupta@ebizneeds.com>
2025-02-21 17:20:27 +0530

    add new chages

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import routes from "./routes/index.route";
+import { Request, Response } from "express";
+import axios from "axios"; // Import Axios
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  console.log
+  res.send("Hello World!"); // Send a simple message in response
+});     
 
 
-async function call(){
-  const updateDetails = await fetch("https://api.github.com/users/defunkt");
-  const data = await updateDetails.json();  
-}
-// dzfbxgfn fb
-call()
+
 app.use("/api", routes);
 
 app.listen(PORT, () => {
